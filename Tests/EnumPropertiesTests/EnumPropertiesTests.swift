ae1e3befb8e3e8458e3c0f493df8d892a5a6bbb3
trimonovds <trimonovds@yandex-team.ru>
2022-04-12 00:11:57 +0300

    Replace snapshots ext from .txt to .swift

--- Tests/EnumPropertiesTests/EnumPropertiesTests.swift
+++ Tests/EnumPropertiesTests/EnumPropertiesTests.swift
@@ -3,15 +3,25 @@ import SwiftSyntax
 import EnumProperties
 import SnapshotTesting
 
-final class EnumPropertiesTests: XCTestCase {
-    func testExample() throws {
-      let fixturesDirURL = URL(fileURLWithPath: String(#file))
-        .deletingLastPathComponent()
-        .appendingPathComponent("Fixtures")
-      let enumsFileURL = fixturesDirURL.appendingPathComponent("Enums.swift")
-      let sourceFile = try SyntaxParser.parse(enumsFileURL)
+extension Snapshotting where Value == URL, Format == String {
+  static let enumProperties: Snapshotting = {
+    var snapshotting: Snapshotting = Snapshotting<String, String>.lines.pullback { url in
+      let sourceFile = try! SyntaxParser.parse(url)
       let visitor = Visitor()
       visitor.walk(sourceFile)
-      assertSnapshot(matching: visitor.output, as: .lines)
+      return visitor.output
+    }
+    snapshotting.pathExtension = "swift"
+    return snapshotting
+  }()
+}
+
+final class EnumPropertiesTests: XCTestCase {
+    func testExample() throws {
+      let url = URL(fileURLWithPath: String(#file))
+        .deletingLastPathComponent()
+        .appendingPathComponent("Fixtures")
+        .appendingPathComponent("Enums.swift")
+      assertSnapshot(matching: url, as: .enumProperties)
     }
 }
